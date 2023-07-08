d4f117856bc85db8f61554dcb98cb9267d574ad6
Rootspring <cheesycod@outlook.com>
2023-07-08 19:54:52 +0000

    fix

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<h1 class="text-6xl font-semibold">TISBMUN 2023</h1>
+
+<div class="mt-4"></div>
+
+<p>
+    TISBMUN 2023 is a once-in-a-lifetime opportunity for students to experience the world of diplomacy and international relations.<br/><br/>
+
+    After a hiatus of two years of online conferences due to the pandemic, the Secretariat is proud and excited to announce that for its twelfth year, TISBMUN will be returning to an in-person format. Bigger, bolder and fresher than ever before, TISBMUN XXII promises to be the most memorable edition yet.<br/><br/>
+
+
+    After a hiatus of two years of online conferences due to the pandemic, the Secretariat is proud and excited to announce that for its twelfth year, TISBMUN will be returning to an in-person format. Bigger, bolder and fresher than ever before, TISBMUN XXII promises to be the most memorable edition yet.<br/><br/>
+</p>
